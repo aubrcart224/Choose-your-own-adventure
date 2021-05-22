c9dae66fda650062a3fb17113e169536ccfa53a0
aubrcart224 <aubrcart224@ed.amdsb.ca>
2021-05-22 18:12:15 -0400

    Add project files.

---
+++ Choose your own adventure/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace Choose_your_own_adventure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.optionLabel1 = new System.Windows.Forms.Label();
+            this.optionLabel2 = new System.Windows.Forms.Label();
+            this.optionLabel3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Segoe UI Symbol", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(12, 9);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(353, 80);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "Output Label";
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.outputLabel.Click += new System.EventHandler(this.outputLabel_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(35, 388);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(64, 93);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // optionLabel1
+            // 
+            this.optionLabel1.Font = new System.Drawing.Font("Segoe UI Symbol", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionLabel1.Location = new System.Drawing.Point(22, 313);
+            this.optionLabel1.Name = "optionLabel1";
+            this.optionLabel1.Size = new System.Drawing.Size(98, 72);
+            this.optionLabel1.TabIndex = 3;
+            this.optionLabel1.Text = "Option Label 1";
+            this.optionLabel1.Click += new System.EventHandler(this.optionLabel1_Click);
+            // 
+            // optionLabel2
+            // 
+            this.optionLabel2.Font = new System.Drawing.Font("Segoe UI Symbol", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionLabel2.Location = new System.Drawing.Point(263, 313);
+            this.optionLabel2.Name = "optionLabel2";
+            this.optionLabel2.Size = new System.Drawing.Size(102, 72);
+            this.optionLabel2.TabIndex = 4;
+            this.optionLabel2.Text = "Option Label 2";
+            this.optionLabel2.Click += new System.EventHandler(this.optionLabel2_Click);
+            // 
+            // optionLabel3
+            // 
+            this.optionLabel3.Font = new System.Drawing.Font("Segoe UI Symbol", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionLabel3.Location = new System.Drawing.Point(147, 313);
+            this.optionLabel3.Name = "optionLabel3";
+            this.optionLabel3.Size = new System.Drawing.Size(92, 66);
+            this.optionLabel3.TabIndex = 5;
+            this.optionLabel3.Text = "Option Label 1";
+            this.optionLabel3.Click += new System.EventHandler(this.optionLabel3_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(163, 388);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(64, 93);
+            this.button3.TabIndex = 6;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.ImageLocation = "mid";
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(15, 73);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 215);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(286, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 93);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(386, 508);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.optionLabel3);
+            this.Controls.Add(this.optionLabel2);
+            this.Controls.Add(this.optionLabel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.outputLabel);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Clash Roayle ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label optionLabel1;
+        private System.Windows.Forms.Label optionLabel2;
+        private System.Windows.Forms.Label optionLabel3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Choose your own adventure/Form1.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace Choose_your_own_adventure
+{
+    public partial class Form1 : Form
+    {
+        //global variables
+        int page = 1;
+        int number;
+        SoundPlayer musicPlayer = new SoundPlayer(Properties.Resources.A_Tone_His_Self_1266414414__1_);
+
+        Random randGen = new Random();
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            //display starting messages
+            outputLabel.Text = " Welcome to clash royale.\nYour oppenet places a ballon on the bridge, do you....";
+            optionLabel1.Text = "play ice golem";
+            optionLabel2.Text = "play hunter";
+            optionLabel3.Text = "";
+            //startup settings
+            button3.Enabled = false;
+            button3.Visible = false;
+            //button2.BackgroundImage.Height.Equals(93);
+            //button2.BackgroundImage.Width.Equals(76);
+            //sound 
+            SoundPlayer player = new SoundPlayer(Properties.Resources.A_Tone_His_Self_1266414414__1_);
+            player.Play();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2) { page = 4; }
+            else if (page == 3) { page = 101; }
+            else if (page == 4) { page = 101; }
+            else if (page == 5) { page = 6; }
+            else if (page == 6) { page = 101; }
+            else if (page == 7) { page = 8; }
+            else if (page == 8) { page = 101; }
+            else if (page == 9) { page = 10; }
+            else if (page == 10) { page = 101; }
+            else if (page == 11) { page = 13; }
+            else if (page == 12) { page = 101; }
+            else if (page == 13) { page = 15; } 
+            else if (page == 14) { page = 101; }
+            else if (page == 15) { page = 101; }
+            else if (page == 16) { page = 18; }
+            else if (page == 17) { page = 101; }
+            else if (page == 18) { page = 20; }
+            else if (page == 19) { page = 101; }
+            else if (page == 20) { page = 101; }
+            DisplayPage();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 3;
+            }
+            else if (page == 2) { page = 5; }
+            else if (page == 3) { page = 1; }
+            else if (page == 4) { page = 1; }
+            else if (page == 5) { page = 7; }
+            else if (page == 6) { page = 1; }
+            else if (page == 7) { page = 9; }
+            else if (page == 8) { page = 1; }
+            else if (page == 9) { page = 11; }
+            else if (page == 10) { page = 1; }
+            else if (page == 11) { page = 12; }
+            else if (page == 12) { page = 1; }
+            else if (page == 13)
+            {
+                number = randGen.Next(1, 11);
+                if (number > 4)
+                {
+                    page = 16;
+                }
+                else
+                {
+                    page = 17;
+                }
+            }
+
+            else if (page == 14) { page = 1; }
+            else if (page == 15) { page = 1; }
+            else if (page == 16) { page = 17; }
+            else if (page == 17) { page = 1; }
+            else if (page == 18) { page = 19; }
+            else if (page == 19) { page = 1; }
+            else if (page == 20) { page = 1; }
+            DisplayPage();
+        }
+
+        public void DisplayPage()
+        {
+            switch (page)
+            {
+                case 1:
+                    outputLabel.Text = " Welcome to clash royale.\nYour oppenet places a ballon on the bridge, do you....";
+                    optionLabel1.Text = "Play ice golem";
+                    optionLabel2.Text = "Play hunter";
+                    optionLabel3.Text = "";
+                    pictureBox1.Image = Properties.Resources.clash_royale_balloon;
+                    button3.Enabled = false;
+                    button3.Visible = false;
+                    break;
+                case 2:
+                    outputLabel.Text = "The hunter kills the ballon\nYour hunter moves forwards do you ";
+                    optionLabel1.Text = "Support it with wall breakers";
+                    optionLabel2.Text = "Support it with miner";
+
+                    pictureBox1.Image = Properties.Resources.images;
+                    SoundPlayer player = new SoundPlayer();
+                    optionLabel3.Text = "";
+                    button3.Enabled = false;
+                    button3.Visible = false;
+
+                    break;
+                case 3:
+                    outputLabel.Text = "Ice golem does not traget air untis.\nThe balloon takeys your tower you lose.\nPlay again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+
+                    pictureBox1.Image = Properties.Resources.cryingcr;
+                    
+                    optionLabel3.Text = "";
+                    button3.Enabled = false;
+                    button3.Visible = false;
+                    
+                    break;
+                case 4:
+                    outputLabel.Text = "Your miner gets easily countered.\nYou lose play again";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    optionLabel3.Text = "";
+                    button3.Enabled = false;
+                    button3.Visible = false;
+
+
+                    pictureBox1.Image = Properties.Resources.cryingcr;
+                    break;
+               
+                case 5:
+                    outputLabel.Text = "They both connect with the tower for big damage\nThe opponent places a mini pekka oppsite lane do you";
+                    optionLabel1.Text = "Kite it with ice goelm";
+                    optionLabel2.Text = "Fireball it";
+                    optionLabel3.Text = "";
+                    button3.Enabled = false;
+                    button3.Visible = false;
+                    pictureBox1.Image = Properties.Resources.mini_pekka;
+                    break;
+                case 6:
+                    outputLabel.Text = "You miss the fireball and the mini pekka takes your tower.\nPlay again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    pictureBox1.Image = Properties.Resources.cryingcr;
+                    optionLabel3.Text = "";
+                    button3.Enabled = false;
+                    button3.Visible = false;
+
+                    break;
+                case 7:
+                    outputLabel.Text = "It gets taken out by your towers\nYou have an elixer advantage do you";
+                    optionLabel1.Text = "Send a miner to thier tower";
+                    optionLabel2.Text = "Wait for them to make a move";
+                    optionLabel3.Text = "";
+                    button3.Enabled = false;
+                    button3.Visible = false;
+                    pictureBox1.Image = Properties.Resources.miner;
+                    break;
+                case 8:
+                    outputLabel.Text = "Never leak elixer if you have the advantage.\nThey take your tower you lose\nPLay again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    pictureBox1.Image = Properties.Resources.cryingcr;
+                    optionLabel3.Text = "";
+                    button3.Enabled = false;
+                    button3.Visible = false;
+
+                    break;
+                case 9:
+                    outputLabel.Text = "Your miner gets lots of chip damage\nThey counter it late with a magic archer\nThey add baloon to the push do you,";
+                    optionLabel1.Text = "Fireball and add spear gobs";
+                    optionLabel2.Text = "Play hunter";
+                    optionLabel3.Text = "";
+                    button3.Enabled = false;
+                    button3.Visible = false;
+                    pictureBox1.Image = Properties.Resources.magic_archer;
+                    break;
+
+                case 10:
+                    outputLabel.Text = " Your hunter kills the ballon but their magic archer kills your hunter.\n They add a mini pekka and take your tower.\nPLay again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    pictureBox1.Image = Properties.Resources.cryingcr;
+                    optionLabel3.Text = "";
+                    button3.Enabled = false;
+                    button3.Visible = false;
+
+                    break;
+                case 11:
+                    //enable button and make it visble 
+                    button3.Enabled = true;
+                    button3.Visible = true;
+                    //noraml code
+                    outputLabel.Text = " It kills the push\n They push unexpecdly with mini pekka in the opposite lane do you";
+                    optionLabel1.Text = "Leave it and push oppsoite lane";
+                    optionLabel2.Text = "Quickly use your sceletons to kill it and wall breaker to support your spear goblins";
+                    optionLabel3.Text = "Counter it with hunter";
+                    pictureBox1.Image = Properties.Resources.wallbreakers;
+                    break;
+                case 12:
+                    outputLabel.Text = "You lose your tower and they kill your push. \nPlay again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    pictureBox1.Image = Properties.Resources.cryingcr;
+                    optionLabel3.Text = "";
+                    button3.Enabled = false;
+                    button3.Visible = false;
+
+                    break;
+                case 13:
+                    outputLabel.Text = "It kills pekka and you push takes the tower.\n You now have an elxir advantage do you,";
+                    optionLabel1.Text = "Send wall breakers to attack"; 
+                    optionLabel2.Text = "Go all in";
+                    optionLabel3.Text = "";
+                    button3.Enabled = false;
+                    button3.Visible = false;
+                    pictureBox1.Image = Properties.Resources.wallbreakers;
+                    // percent
+                    //int number;
+                    //number = randGen.Next(1, 11);
+                    //if (number > 0)
+                    //{
+                    //    page = 16;
+                    //}
+                    //else
+                    //{
+                    //    page = 17;
+                    //}
+
+
+                    break;
+                case 14:
+                    outputLabel.Text = "It kill your hunter.\n Now ahead in elxir they push and you cant defend.\n PLay again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    pictureBox1.Image = Properties.Resources.cryingcr;
+                    optionLabel3.Text = "";
+                    button3.Enabled = false;
+                    button3.Visible = false;
+
+                    break;
+                case 15:
+                    outputLabel.Text = "Your opponent counters your pushes and takes your tower because you over commited.\n Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    pictureBox1.Image = Properties.Resources.cryingcr;
+                    optionLabel3.Text = "";
+                    button3.Enabled = false;
+                    button3.Visible = false;
+
+                    break;
+                case 16:
+                    outputLabel.Text = "They miss thier log and it works. \n They play baloon but you dont have hunter in hand do you,";
+                    optionLabel1.Text = "Counter with spear goblins";
+                    optionLabel2.Text = "Hit it with a well timed fireball";
+                    optionLabel3.Text = "";
+                    button3.Enabled = false;
+                    button3.Visible = false;
+                    pictureBox1.Image = Properties.Resources.clash_royale_balloon;
+                    break;
+                case 17:
+                    outputLabel.Text = "They take it out easily with a zap.\n The ballon taskes your tower.Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    pictureBox1.Image = Properties.Resources.cryingcr;
+                    optionLabel3.Text = "";
+                    button3.Enabled = false;
+                    button3.Visible = false;
+
+                    break;
+                case 18:
+                    outputLabel.Text = "You succesfully defend the baloon.\n They drop a gaint and a magic archer on the bridge";
+                    optionLabel1.Text = "Leave the push and take the other tower";
+                    optionLabel2.Text = "Use a hunter to defeend and counter push";
+                    optionLabel3.Text = "";
+                    button3.Enabled = false;
+                    button3.Visible = false;
+                    pictureBox1.Image = Properties.Resources.gaint_baloon;
+                    break;
+                case 19:
+                    outputLabel.Text = "They take out your crown tower and you lose.\n Play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    pictureBox1.Image = Properties.Resources.cryingcr;
+                    optionLabel3.Text = "";
+                    button3.Enabled = false;
+                    button3.Visible = false;
+
+                    break;
+                case 20:
+                    outputLabel.Text = "You take thier crown tower.\n Winner!\n Would you like to play again?";
+                    optionLabel1.Text = "Yes";
+                    optionLabel2.Text = "No";
+                    optionLabel3.Text = "";
+                    button3.Enabled = false;
+                    button3.Visible = false;
+                    pictureBox1.Image = Properties.Resources.winner_cr;
+
+                    break;
+                case 101:
+                    outputLabel.Text = "Thanks For Playing";
+                    optionLabel1.Text = "";
+                    optionLabel2.Text = "";
+                    optionLabel3.Text = "";
+                    Refresh();
+                        Thread.Sleep(1500);
+                        Application.Exit();
+                    break;
+                default:
+                    break;
+                 
+            }
+
+
+        }
+
+        private void optionLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //button3.Enabled = true;
+            page = 14;
+            DisplayPage();
+               
+                
+        }
+
+        private void optionLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void optionLabel3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void outputLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
